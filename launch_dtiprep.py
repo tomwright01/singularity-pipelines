64cd790b19d2d09cadd1e15a0b5a717ad6fa7e20
Tom Wright <tom@maladmin.com>
2017-02-24 16:06:38 -0500

    Fixed launcher

--- launch_dtiprep.py
+++ launch_dtiprep.py
@@ -7,18 +7,19 @@ import logging
 import argparse
 import os
 import tempfile
+import sys
 import subprocess
+import socket
 
 CONTAINER = '/projects/twright/bootstrap_qa.img'
 
 JOB_TEMPLATE = """
 #####################################
 #$ -S /bin/bash
-#$ -cwd
+#$ -wd /tmp/
 #$ -N {name}
 #$ -e {errfile}
 #$ -o {logfile}
-#$ -pe make {slots}
 #####################################
 echo "------------------------------------------------------------------------"
 echo "Job started on" `date`
@@ -40,25 +41,23 @@ class QJob(object):
 
     def __enter__(self):
         self.qs_f, self.qs_n = tempfile.mkstemp(suffix='.qsub')
-        self.sh_f, self.sh_n = tempfile.mkstemp(suffix='.sh')
+        return self
 
-    def __exit__(self):
+    def __exit__(self, type, value, traceback):
         try:
             os.close(self.qs_f)
-            os.close(self.sh_f)
             if self.cleanup:
                 os.remove(self.qs_n)
-                os.remove(self.sh_n)
         except OSError:
             pass
 
     def run(self, code, name="job", logfile="output.$JOB_ID", errfile="error.$JOB_ID", cleanup=True, slots=1):
-        open(self.sh_f).write(code)
-        open(self.qs_f).write(JOB_TEMPLATE.format(script=self.sh_n,
-                                                  name=name,
-                                                  logfile=logfile,
-                                                  errfile=errfile,
-                                                  slots=slots))
+        open(self.qs_n, 'w').write(JOB_TEMPLATE.format(script=code,
+                                                       name=name,
+                                                       logfile=logfile,
+                                                       errfile=errfile,
+                                                       slots=slots))
+        logger.info('Submitting job')
         subprocess.call('qsub < ' + self.qs_n, shell=True)
 
 
@@ -71,7 +70,9 @@ def make_job(src_dir, dst_dir, nrrd_name, cleanup=True):
                     nrrd_name=nrrd_name))
 
     with QJob() as qjob:
-        qjob.run(code=code)
+        logfile = '{}:/tmp/output.$JOB_ID'.format(socket.gethostname())
+        errfile = '{}:/tmp/error.$JOB_ID'.format(socket.gethostname())
+        qjob.run(code=code, logfile=logfile, errfile=errfile)
 
 
 def process_nrrd(src_dir, dst_dir, nrrd_file):
@@ -86,16 +87,27 @@ def process_nrrd(src_dir, dst_dir, nrrd_file):
     make_job(src_dir, dst_dir, nrrd_file)
 
 
-def process_session(nrrd_dir, dtiprep_dir, session):
+def process_session(src_dir, out_dir, session):
     """Launch DTI prep on all nrrd files in a directory"""
-    src_dir = os.path.join(nrrd_dir, session)
-    out_dir = os.path.join(dtiprep_dir, session)
+    src_dir = os.path.join(src_dir, session)
+    out_dir = os.path.join(out_dir, session)
     nrrds = [f for f in os.listdir(src_dir) if f.endswith('.nrrd')]
 
-    if not nrrds:
-        logger.warning('No nrrd files found for session:{}'.format(session))
-        return
+    # Who knew, not all nrrd files are DTI's
+    nrrd_dti = []
+    for f in nrrds:
+        try:
+            _, tag, _, _ = datman.scanid.parse_filename(f)
+        except datman.scanid.ParseException:
+            continue
 
+        if 'DTI' in tag:
+            nrrd_dti.append(f)
+
+    if not nrrd_dti:
+        logger.warning('No DTI nrrd files found for session:{}'.format(session))
+        return
+    logger.info('Found {} DTI nrrd files.'.format(len(nrrd_dti)))
     if not os.path.isdir(out_dir):
         try:
             os.mkdir(out_dir)
@@ -104,27 +116,28 @@ def process_session(nrrd_dir, dtiprep_dir, session):
                          .format(out_dir))
             return
 
-    for nrrd in nrrds:
+    for nrrd in nrrd_dti:
         process_nrrd(src_dir, out_dir, nrrd)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Run DTIPrep on a DTI File")
     parser.add_argument("study", help="Study")
-    parser.add_argument("--session", dest="session", help="dicom directory")
+    parser.add_argument("--session", dest="session", help="Session identifier")
     parser.add_argument("--outDir", dest="outDir", help="output directory")
     args = parser.parse_args()
 
-    cfg = datman.config.config(args.study)
+    cfg = datman.config.config(study=args.study)
 
     nrrd_path = cfg.get_path('nrrd')
-    pipeline_path = cfg.get_path('pipeline')
+    pipeline_path = cfg.get_path('dtiprep')
 
     if not os.path.isdir(pipeline_path):
-        os.makedir(pipeline_path)
+        logger.info("Creating output path:{}".format(pipeline_path))
+        os.mkdir(pipeline_path)
 
     if not os.path.isdir(nrrd_path):
         logger.error("Src directory:{} not found".format(nrrd_path))
-        return
+        sys.exit(1)
 
     if not args.session:
         sessions = [os.path.join(nrrd_path, d) for d
@@ -134,4 +147,4 @@ if __name__ == '__main__':
         sessions = [args.session]
 
     for session in sessions:
-        process_session(session)
+        process_session(nrrd_path, pipeline_path, session)
